573f78c0dd8273026f29932134d6f910c595aa27
Gustavo Rodrigues <gustavolourenco821@gmail.com>
2024-05-14 08:35:37 -0300

    add: classe para guardar os obstaculos

---
+++ obstaculos.py
@@ -0,0 +1,33 @@
+import pygame
+import random
+
+class Obstaculos:
+    def __init__(self,arquivo_da_img,alturaIMG,larguraIMG,x_inicial,y_inicial, velocidade) -> None:
+        self.imagem = pygame.image.load(arquivo_da_img)
+        
+        self.largura = larguraIMG
+        self.altura = alturaIMG
+
+        self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
+
+        self.pos_x = x_inicial
+        self.pos_y = y_inicial
+        self.velocidade = velocidade    
+        self.mascara = pygame.mask.from_surface(self.imagem)
+    
+    def desenho(self, tela):
+        tela.blit(self.imagem,(self.pos_x,self.pos_y))
+
+    def velocidades(self):
+        velocidade = random.randint(1,10)
+        return velocidade
+
+    def movimento_inimigo(self):
+        self.pos_x += self.velocidade
+        if self.pos_x > 800:
+            self.pos_x =0
+    
+    def movimento_inimigo2(self):
+        self.pos_y += self.velocidade
+        if self.pos_y >800:
+            self.pos_y =0
