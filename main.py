85b3727a26987ce7678f2f000356da3ec9af01be
Gustavo Rodrigues <gustavolourenco821@gmail.com>
2024-05-14 10:17:13 -0300

    add: MUSICA + VIDAS

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jogador import *
 from obstaculos import *
 from objetivos import *
 import time
+import os
 pygame.init()
 # Definir as cores
 pontuacao = 0
@@ -14,8 +15,15 @@ pygame.display.set_caption("Fruit Drops") #Alterando o nome do jogo
 clock = pygame.time.Clock() #Limitando o FPS
 tela.fill((255,0,0)) #escolhendo a cor de fundo do jogo
 #Configurando a fonte
-fonte = pygame.font.SysFont(None,36)
+fonte = pygame.font.SysFont('Berlin sans FB Demi',36)
 
+# Definição do diretório onde está a música
+
+# Carregamento da música
+pygame.mixer.music.load('musica.mp3')
+
+# Reprodução da música em loop (-1 para loop infinito)
+pygame.mixer.music.play(-1)
 
 rodando = True #fazendo o jogo rodar infinitamente
 mario = Player('mario.png',50,50,350,500)
@@ -47,6 +55,7 @@ lista_bonus = [Objetivos('cogumelo_verde.png',25,30,0),
                Objetivos('cogumelo_verde.png',25,30,0),
                Objetivos('cogumelo_verde.png',25,30,0),]
 fundo = Fundo('fundoMario.jpg',500,800,0,0)
+marioWin = Fundo('marioWIn.png',400,170,100,100)
 while True:
     for evento in pygame.event.get():          
         if evento.type == pygame.QUIT:
@@ -77,15 +86,24 @@ while True:
         if mario.mascara.overlap(inimigos.mascara,(inimigos.pos_x-mario.pos_x , mario.pos_y-inimigos.pos_y)):
             inimigos.pos_x = random.randint(50,750)
             inimigos.velocidade = random.randint(1,10)
-            pontuacao -= 1
+            vidas -=1
             mario.pos_x = 350
             mario.pos_y = 500
             inimigos.pos_y = 0
-
-    texto_pontuacao = fonte.render(f"Pontuação: {pontuacao}",True,(0,255,0))
+    
+    texto_pontuacao = fonte.render(f"Pontuação: {pontuacao}",True,(0,0,0))
+    texto_vidas = fonte.render(f'Vidas: {vidas}',True,(0,0,0))
     tela.blit(texto_pontuacao,(0,10))
-    
-    
+    tela.blit(texto_vidas,(0,40))
+    texto_gameOver = fonte.render("Game Over", True,(0,0,0))
+    texto_WIN = fonte.render("Você venceu", True,(0,0,0))
+    if vidas == 0:
+        fundo.desenho(tela)
+        tela.blit(texto_gameOver,(320,200))
+    if pontuacao == 100 or pontuacao >100:
+        fundo.desenho(tela)
+        tela.blit(texto_WIN,(320,200))
+        marioWin.desenho(tela)
     pygame.display.update()
     clock.tick(60)
     
