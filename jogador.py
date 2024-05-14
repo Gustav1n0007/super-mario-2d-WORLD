0bbc0642170443d66a0e28783b360f99641a2190
Gustavo Rodrigues <gustavolourenco821@gmail.com>
2024-05-14 09:47:35 -0300

    add: classe para a imagem de fundo

--- jogador.py
+++ jogador.py
@@ -30,5 +30,22 @@ class Player:
             self.pos_y = -0
         if self.pos_y > 400:
             self.pos_y = 400
-        if self.pos_x > 680:
-            self.pos_x = 680
+        if self.pos_x > 750:
+            self.pos_x = 750
+
+class Fundo:
+    def __init__(self,arquivo_da_img,alturaIMG,larguraIMG,x_inicial,y_inicial) -> None:
+        self.imagem = pygame.image.load(arquivo_da_img)
+        
+        self.largura = larguraIMG
+        self.altura = alturaIMG
+
+        self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
+
+        self.pos_x = x_inicial
+        self.pos_y = y_inicial
+    
+        self.mascara = pygame.mask.from_surface(self.imagem)
+
+    def desenho(self, tela):
+        tela.blit(self.imagem,(self.pos_x,self.pos_y))
