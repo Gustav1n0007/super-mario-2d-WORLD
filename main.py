6dacf8215ef3b024542033e5906dcadfc7314618
Gustavo Rodrigues <gustavolourenco821@gmail.com>
2024-05-14 08:42:52 -0300

    add: pontuacao na tela

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ tela = pygame.display.set_mode((800,500)) #Escolhendo o tamanho da tela
 pygame.display.set_caption("Fruit Drops") #Alterando o nome do jogo
 clock = pygame.time.Clock() #Limitando o FPS
 tela.fill((255,0,0)) #escolhendo a cor de fundo do jogo
+#Configurando a fonte
+fonte = pygame.font.SysFont(None,36)
+
 
 rodando = True #fazendo o jogo rodar infinitamente
 mario = Player('mario.png',75,75,350,500)
@@ -21,14 +24,16 @@ lista_inimigos = [Obstaculos('casco.png',30,30,350,0,3),
                   Obstaculos('casco.png',30,30,400,0,5),
                   Obstaculos('casco.png',30,30,100,0,4)]
 
-lista_objetivos = [Objetivos('cogumelo.png',30,30,320,0,2),
-                   Objetivos('cogumelo.png',30,30,250,0,8),
-                   Objetivos('cogumelo.png',30,30,150,0,7),
-                   Objetivos('cogumelo.png',30,30,60,0,6),
-                   Objetivos('cogumelo.png',30,30,400,0,3),
-                   Objetivos('cogumelo.png',30,30,450,0,2),
-                   Objetivos('cogumelo.png',30,30,500,0,2),
-                   Objetivos('cogumelo.png',30,30,650,0,2)]
+lista_objetivos = [Objetivos('cogumelo.png',30,40,320,0,2),
+                   Objetivos('cogumelo.png',30,40,250,0,8),
+                   Objetivos('cogumelo.png',30,40,150,0,7),
+                   Objetivos('cogumelo.png',30,40,60,0,6),
+                   Objetivos('cogumelo.png',30,40,400,0,3),
+                   Objetivos('cogumelo.png',30,40,450,0,5),
+                   Objetivos('cogumelo.png',30,40,500,0,6),
+                   Objetivos('cogumelo.png',30,40,650,0,3),
+                   Objetivos('cogumelo.png',30,40,700,0,2),
+                   Objetivos('cogumelo.png',30,40,760,0,4)]
 
 while True:
     for evento in pygame.event.get():          
@@ -40,14 +45,15 @@ while True:
     for cogumelos in lista_objetivos:
         cogumelos.movimento_obj()
         cogumelos.desenho(tela)
-        if mario.mascara.overlap(cogumelos.mascara,(cogumelos.pos_x-mario.pos_x , mario.pos_y-mario.pos_y)):
+        if mario.mascara.overlap(cogumelos.mascara,(cogumelos.pos_x-mario.pos_x , mario.pos_y-cogumelos.pos_y)):
             pontuacao +=1
     for inimigos in lista_inimigos:
         inimigos.movimento_inimigo2()
         inimigos.desenho(tela)
-        if mario.mascara.overlap(inimigos.mascara,(inimigos.pos_x-mario.pos_x , mario.pos_y-mario.pos_y)):
+        if mario.mascara.overlap(inimigos.mascara,(inimigos.pos_x-mario.pos_x , mario.pos_y-inimigos.pos_y)):
             pontuacao -= 1
-    
+    texto_pontuacao = fonte.render(f"Pontuação: {pontuacao}",True,(0,255,0))
+    tela.blit(texto_pontuacao,(0,10))
    
     
     pygame.display.update()
